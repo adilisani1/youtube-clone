f5db5b1b2f7dc98635777e15c6a8c723a92fd6bb
Adil Isani <adil.isani1@gmail.com>
2024-10-16 22:50:23 +0500

    Sidebar completed

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+import { categories } from '../utils/constants';
+
+export const Sidebar = () => {
+    const selectedCat = "New"
+    return (
+        <Stack sx={{
+            overflowY: "auto",
+            height: { sx: 'auto', md: '95%' },
+            flexDirection: { md: 'column' }
+        }}>
+            {
+                categories.map((category) => (
+                    <button className='category-btn' style={{
+                        background: category.name === selectedCat && '#fc1503 ', color: ' #fff'
+                    }}
+                        key={category.name}>
+                        <span
+                            style={{ color: category.name === selectedCat ? 'white' : 'red', marginRight: '15px' }} >{category.icon}</span>
+                        <span style={{ opacity: category.name === selectedCat ? '1' : '0.8' }}>{category.name}</span>
+                    </button>
+                ))
+            }
+        </Stack>
+    )
+}
